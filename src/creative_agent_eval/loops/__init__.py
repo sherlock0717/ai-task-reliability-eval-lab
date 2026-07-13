8b0bb4a45d50a8b6cdaba92acc5736eb8fb67e39
海澄 <2235829994@qq.com>
2026-07-13 21:26:59 +0800

    export all loop implementations

--- src/creative_agent_eval/loops/__init__.py
+++ src/creative_agent_eval/loops/__init__.py
@@ -1,3 +1,11 @@
+from .critique_revise import CritiqueReviseLoop
+from .divergent_convergent import DivergentConvergentLoop
 from .one_shot import OneShotLoop
+from .tool_grounded import ToolGroundedLoop
 
-__all__ = ["OneShotLoop"]
+__all__ = [
+    "CritiqueReviseLoop",
+    "DivergentConvergentLoop",
+    "OneShotLoop",
+    "ToolGroundedLoop",
+]
