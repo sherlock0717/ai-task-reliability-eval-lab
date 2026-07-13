60444aea3300d576b22b203da10bf2748ac23b23
海澄 <2235829994@qq.com>
2026-07-13 21:18:01 +0800

    add provider package

---
+++ src/creative_agent_eval/providers/__init__.py
@@ -0,0 +1,4 @@
+from .base import ModelProvider, ProviderRequest, ProviderResponse
+from .scripted import ScriptedProvider
+
+__all__ = ["ModelProvider", "ProviderRequest", "ProviderResponse", "ScriptedProvider"]
