0b348ebb90a5abb6a76e48ce8ae35f68f8f77a3e
海澄 <2235829994@qq.com>
2026-07-13 21:22:04 +0800

    export DeepSeek provider

--- src/creative_agent_eval/providers/__init__.py
+++ src/creative_agent_eval/providers/__init__.py
@@ -1,4 +1,12 @@
 from .base import ModelProvider, ProviderRequest, ProviderResponse
+from .deepseek import DeepSeekConfig, DeepSeekProvider
 from .scripted import ScriptedProvider
 
-__all__ = ["ModelProvider", "ProviderRequest", "ProviderResponse", "ScriptedProvider"]
+__all__ = [
+    "DeepSeekConfig",
+    "DeepSeekProvider",
+    "ModelProvider",
+    "ProviderRequest",
+    "ProviderResponse",
+    "ScriptedProvider",
+]
